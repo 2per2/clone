19ea880b32565533638400b907be2da136346453
melonhong <melonhong46@gmail.com>
2025-11-25 14:26:39 +0900

    feat: Tailwind CSS 및 스타일 적용
    - Tailwind CSS 패키지 추가 및 설정
    - index.html에 CSS 파일 링크 추가
    - App 컴포넌트에 Tailwind CSS 클래스를 사용하여 스타일 적용
    - 기존 CSS 파일을 Tailwind CSS로 대체

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import "./index.css";
-
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <h1 className="text-red-500 text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </>
   );
 }
